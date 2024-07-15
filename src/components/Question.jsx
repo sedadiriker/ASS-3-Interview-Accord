b2d31870202bcf3e7fc355fe573c70fab8a88d24
seda <seda.diriker@gmail.com>
2024-07-15 11:00:39 +0300

    clickArrow added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import questions from "../helper/data.js";
 import { arrowdown, arrowup } from "../helper/icons.js";
 
 const Question = () => {
+  const [show, setShow] = useState(false);
+
+  const clickArrow = () => {
+    setShow(!show);
+  };
   return (
     <div className="container">
-      {questions.map(({ question, id }) => (
-        <p className="question" key={id}>
-          {id}. {question}
-          <span className="arrow">{arrowdown}</span>
-        </p>
-      ))}
+      {questions.map(({ question, id, answer }) =>
+        show ? (
+          <div className="question" key={id}>
+            {id}. {question}
+            <span onClick={clickArrow} className="arrow">{arrowdown}</span>
+            <p>{answer}</p>
+          </div>
+        ) : (
+          <div className="question" key={id}>
+            {id}. {question}
+            <span onClick={clickArrow} className="arrow">{arrowdown}</span>
+          </div>
+        )
+      )}
     </div>
   );
 };
