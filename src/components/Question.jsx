5fa0c7d916fa3d601d3d5a977cc88bf1d6ea3c9b
seda <seda.diriker@gmail.com>
2024-07-15 10:54:48 +0300

    arrow added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import questions from "../helper/data"
+import React from "react";
+import questions from "../helper/data.js";
+import { arrowdown, arrowup } from "../helper/icons.js";
 
 const Question = () => {
   return (
     <div className="container">
-      {
-        questions.map(({question,id}) => (
-            <p className="question" key={id}>{id}. {question}</p>
-        ))
-      }
+      {questions.map(({ question, id }) => (
+        <p className="question" key={id}>
+          {id}. {question}
+          <span className="arrow">{arrowdown}</span>
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
